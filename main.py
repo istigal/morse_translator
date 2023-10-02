52ee2f2e46d4417379e1fcc28b9c96f785a7cfa1
Istvan G <istw4n@gmail.com>
2023-10-02 15:43:49 +0300

    First commit

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-              'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '&', "'", '@', ')', '(',
-              ':', ',', '=', '!', '.', '-', '×', '%', '+', '"', '?', '/']
-morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---',
-         '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', '-----', '.----', '..---',
-         '...--', '....-', '.....', '-....', '--...', '---..', '----.', '.-...', '.----.', '.--.-.', '-.--.-', '-.--.',
-         '---...', '--..--', '-...-', '-.-.--', '.-.-.-', '-....-', '-..-', '.-.-.', '.-..-.', '..--..', '-..-.',
-         '.--.-']
+characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',    'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '&', "'", '@', ')', '(', ':', ',', '=', '!', '.', '-', '×', '%', '+', '"', '?', '/']
+morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', '-----', '.----', '..---', '...--', '....-', '.....', '-....', '--...', '---..', '----.', '.-...', '.----.', '.--.-.', '-.--.-', '-.--.', '---...', '--..--', '-...-', '-.-.--', '.-.-.-', '-....-', '-..-', '.-.-.', '.-..-.', '..--..', '-..-.', '.--.-']
 
 
 def encode():
@@ -17,28 +11,29 @@ def encode():
         else:
             res.append(f"{morse[characters.index(char)]} ")
     result = ''.join(res)
-    print(result)
+    print("The morse text is:\n\n"+result)
 
 
 def decode():
     code = input("Add the morse code to decode: ").split(' ')
     res = []
     for char in code:
-        if char == '/':
+        if char not in morse:
+            print("Please type only morse code!")
+            return
+        elif char == '/':
             res.append(' ')
         elif char == '':
             continue
-        elif char not in morse:
-            print("Please type only morse code.")
         else:
             res.append(characters[morse.index(char)])
     result = ''.join(res)
-    print(result)
+    print(f"The translated text is:\n\n{result}")
 
 
 method = ''
 while method.upper() != "EXIT":
-    method = input("Do you want to encode(E) or decode(D) morse code (type E/D or type EXIT to leave): ")
+    method = input("\nDo you want to encode(E) or decode(D) text? (type E/D or type EXIT to leave): ")
     if method.upper() == "E":
         encode()
     elif method.upper() == "D":
